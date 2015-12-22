3d22b0f6dab22eb57d363693725d2a7a80cb23dc
Dan Black <dblack@boardbooks.com>
2015-12-22 13:24:52 +1300

    [Add] Xmas carol

---
+++ XmasCodeKata8/Controllers/TwelveDaysCarol.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace XmasCodeKata8.Controllers
+{
+    public class TwelveDaysCarol
+    {
+        private readonly List<string> _days = new List<string>();
+
+        public TwelveDaysCarol()
+        {
+            _days.Add("Partridge in a Pear Tree");
+            _days.Add("Two Turtle Doves");
+            _days.Add("Three French Hens");
+            _days.Add("Four 4 Calling Birds");
+            _days.Add("Five Gold Rings");
+            _days.Add("Six Geese a-Laying");
+            _days.Add("Seven Swans a-Swimming");
+
+        }
+        public string Day(int day)
+        {
+            if (day < 1 || day > _days.Count)
+                return "Nothing";
+
+            return _days[day - 1];
+        }
+    }
+}
---
+++ XmasCodeKata8/Controllers/XmasController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace XmasCodeKata8.Controllers
+{
+    public class XmasController : ApiController
+    {
+        [HttpGet]
+        public string OnTheNthDay(int day)
+        {
+            var carol = new TwelveDaysCarol();
+            return carol.Day(day);
+        }
+    }
+}
